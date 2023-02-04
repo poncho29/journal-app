5a202fa2a6d7f8e85f465a9384528c5d35c1ad6e
poncho29 <meneses0828@gmail.com>
2023-02-04 15:05:50 -0500

    added ui material library

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,11 @@
+import { MailOutline } from "@mui/icons-material"
+import { Typography } from "@mui/material"
 
 export const JournalPage = () => {
   return (
-    <div>JounalPage</div>
+    <>
+      <Typography variant='h1'>Journal Page</Typography>
+      <MailOutline />
+    </>
   )
 }
