6e40665b31d7a55e62c2de8c90a927b4a4f66f52
poncho29 <meneses0828@gmail.com>
2023-02-04 14:45:20 -0500

    initial structure and paths

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,6 @@
+
+export const JournalPage = () => {
+  return (
+    <div>JounalPage</div>
+  )
+}
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { JournalRoutes } from '../journal/routes/JournalRoutes';
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {/* Login & Registro */}
+      <Route path='auth/*' element={ <AuthRoutes /> } />
+
+      {/* Journal App */}
+      <Route path='/*' element={ <JournalRoutes /> } />
+    </Routes>
+  )
+}
